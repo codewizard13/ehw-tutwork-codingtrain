1a7a329bb24e358d8f7155650d0b627749f2bb1e
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-22 13:49:42 -0600

    ADD app.js:  File header only, no code yet

---
+++ 9.15_2d_arrays/app.js
@@ -0,0 +1,13 @@
+/*
+Program Type:     TUTWRK
+Platform/Channel: YouTube/The Coding Train
+Programmer:       Eric Hepperle
+Date Created:     2022-12-22
+
+Purpose: Demonstrate multidimensional arrays in JavaScript
+
+*/
+
+// ADD code or delete
+
+
